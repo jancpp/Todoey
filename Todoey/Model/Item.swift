0ea21a80b5ec465f259467ee22683bb3ea2d6c5b
Jan Polzer <janman74@gmail.com>
2018-07-12 10:33:03 -0500

    Added Item.swift to have MVC model

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jan Polzer on 7/12/18.
+//  Copyright © 2018 Apps KC. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title: String = ""
+    var done: Bool = false
+}
